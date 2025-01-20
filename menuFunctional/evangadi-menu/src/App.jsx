77404ba49b3d95cc2f3d03885359f136470fec66
gechjs <gizachew980@gmail.com>
2025-01-20 11:16:19 +0300

    refactor

--- menuFunctional/evangadi-menu/src/App.jsx
+++ menuFunctional/evangadi-menu/src/App.jsx
@@ -1,28 +1,25 @@
 import "./App.css";
 import "./commonResource/data";
-import { Component } from "react";
 import FoodItem from "./Components/FoodItem";
 import Menu from "./commonResource/data";
 
 function App() {
   return (
-    <div className="all-container ">
+    <div className="all-container">
       <header className="title">
         <h1>Evangadi Menu</h1>
         <div></div>
       </header>
       <div className="foods-container">
-        {Menu.map((obj) => {
-          return (
-            <FoodItem
-              key={obj.id}
-              price={obj.price}
-              desc={obj.desc}
-              title={obj.title}
-              img={obj.img}
-            />
-          );
-        })}
+        {Menu.map(({ id, price, desc, title, img }) => (
+          <FoodItem
+            key={id}
+            price={price}
+            desc={desc}
+            title={title}
+            img={img}
+          />
+        ))}
       </div>
     </div>
   );
