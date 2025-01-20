af9099daef90200fd8508fbaca1ea5392b3c0007
gechjs <gizachew980@gmail.com>
2025-01-20 11:17:41 +0300

    added new food item

--- menuFunctional/evangadi-menu/src/App.jsx
+++ menuFunctional/evangadi-menu/src/App.jsx
@@ -1,7 +1,38 @@
 import "./App.css";
 import "./commonResource/data";
 import FoodItem from "./Components/FoodItem";
-import Menu from "./commonResource/data";
+
+
+const Menu = [
+  {
+    id: 1,
+    price: "$10",
+    desc: "Delicious pasta with creamy sauce",
+    title: "Pasta Alfredo",
+    img: "pasta-alfredo.jpg",
+  },
+  {
+    id: 2,
+    price: "$12",
+    desc: "Savory grilled chicken with herbs",
+    title: "Grilled Chicken",
+    img: "grilled-chicken.jpg",
+  },
+  {
+    id: 3,
+    price: "$8",
+    desc: "Classic cheeseburger with fresh ingredients",
+    title: "Cheeseburger",
+    img: "cheeseburger.jpg",
+  },
+  {
+    id: 4,
+    price: "$5",
+    desc: "Crispy golden french fries",
+    title: "French Fries",
+    img: "french-fries.jpg",
+  },
+];
 
 function App() {
   return (
